3b05b1efc8f5391d1d54119d4aa13f708fbe5d76
BT_3W2_15 <alvinl70@nycstudents.net>
2022-11-14 09:25:59 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        for (int x = 0; x <= 10; x++) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        for (int x = 0; x < 11; x++) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        for (int x = 15; x >= 0; x--) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        for (int x = 15; x > -1; x--) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        for (int x = 1; x <= 15; x++) {
+            if (x % 2 == 1) {
+                System.out.print(x + " ");
+            }
+        }
+        System.out.println();
+
+        for (int x = 1; x <= 15; x += 2) {
+            System.out.print(x + " ");
+        }
+    }
 }
--- src/Main2.java
+++ src/Main2.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Main2 {
+public class Main2 {
+    public static void main(String[] args) {
+        for (int count = 5; count < 10; count++) {
+            System.out.print(count + " ");
+        }
+        // 1a
+        System.out.println();
+        for (int i = 20; i >= 0; i = i - 3) {
+            System.out.print(i + " ");
+        }
+        //1b
+        System.out.println();
+        int sum = 0;
+        int x = 5;
+        for (int i = 0; i < 10 + x; i += 2) {
+            sum += i;
+        }
+        System.out.println(sum);
+        //1c
+        int j = 40;
+        for (int i = 10; i < j + 10; i += 5) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i <= 100 ; i += 6) {
+            System.out.print(i + " ");
+        }
+
+
+    }
 }
